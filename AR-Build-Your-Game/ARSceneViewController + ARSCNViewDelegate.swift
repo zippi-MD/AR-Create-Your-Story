edadb548a5b49ed9007d612512e40e2195733ac5
Alejandro Mendoza <zippi180@gmail.com>
2019-06-23 18:18:11 -0500

    Implemented AR Scene

---
+++ AR-Build-Your-Game/ARSceneViewController + ARSCNViewDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ARSceneViewController + ARSCNViewDelegate.swift
+//  AR-Build-Your-Game
+//
+//  Created by Alejandro Mendoza on 6/23/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import Foundation
+import ARKit
+
+extension ARSceneViewController: ARSCNViewDelegate {
+    
+}
---
+++ AR-Build-Your-Game/ARSceneViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ARSceneViewController.swift
+//  AR-Build-Your-Game
+//
+//  Created by Alejandro Mendoza on 6/23/19.
+//  Copyright © 2019 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ARSceneViewController: UIViewController {
+    
+    var sceneView: ARSCNView!
+    
+    override func loadView() {
+        super.loadView()
+        
+        sceneView = ARSCNView(frame: view.bounds)
+        
+        view.addSubview(sceneView)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupARConfiguration()
+    }
+    
+    func setupARConfiguration(){
+        let configuration = ARWorldTrackingConfiguration()
+        
+        sceneView.session.run(configuration)
+        sceneView.delegate = self
+        
+        #if DEBUG
+        
+        sceneView.debugOptions = [.showFeaturePoints]
+        
+        #endif
+    }
+
+}
