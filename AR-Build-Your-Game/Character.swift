5789d3328b4d50aac2c9b18d5aba10f3a7183837
Carlos Daniel Hernandez Chauteco <gamercarlos1530@comunidad.unam.mx>
2019-06-24 17:26:38 -0500

    -added audios

--- AR-Build-Your-Game/Character.swift
+++ AR-Build-Your-Game/Character.swift
@@ -8,7 +8,21 @@
 
 import SceneKit
 
+enum CharacterSounds {
+    case ahhSound
+    case catchFireSound
+    case ouchSound
+    case hitSound
+    case hitEnemySound
+    case explodeEnemySound
+    case jumpSound
+    case attackSound
+}
+
 class Character: SCNNode {
+    //Mark variables
+    private var sounds = [CharacterSounds: SCNAudioSource]()
+    //
     var isJumping: Bool = false {
         didSet {
             
@@ -19,6 +33,7 @@ class Character: SCNNode {
         super.init()
         loadModel()
         loadParticles()
+        loadSounds()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -49,5 +64,39 @@ class Character: SCNNode {
     
     private func loadParticles() {
         
+    }
+    
+    private func loadSounds() {
+        sounds[.ahhSound] = loadSound(named: "aah_extinction", fileExtention: "mp3", volume: 1.0)
+        
+        sounds[.catchFireSound] = loadSound(named: "panda_catch_fire", fileExtention: "mp3", volume: 5.0)
+        
+        sounds[.ouchSound] = loadSound(named: "ouch_firehit", fileExtention: "mp3", volume: 2.0)
+        
+        sounds[.hitSound] = loadSound(named: "hit", fileExtention: "mp3", volume: 2.0)
+        
+        sounds[.hitEnemySound] = loadSound(named: "Explosion1", fileExtention: "m4a", volume: 2.0)
+        
+        sounds[.explodeEnemySound] = loadSound(named: "Explosion2", fileExtention: "m4a", volume: 2.0)
+        
+        sounds[.jumpSound] = loadSound(named: "jump", fileExtention: "m4a", volume: 0.2)
+        
+        sounds[.attackSound] = loadSound(named: "attack", fileExtention: "mp3", volume: 1.0)
+    }
+    
+    //Utils
+    
+    private func loadSound(named: String, fileExtention: String,volume: Float) -> SCNAudioSource?{
+        guard let sound = SCNAudioSource(named: "audio/\(named).\(fileExtention)") else { return nil}
+        sound.volume = volume
+        sound.isPositional = false
+        sound.load()
+        
+        return sound
+    }
+    
+    private func playSound(sound: CharacterSounds) {
+        guard let aux = sounds[sound], let maxNode = self.childNode(withName: "Max_rootNode", recursively: true) else { return }
+        maxNode.runAction(SCNAction.playAudio(aux, waitForCompletion: false))
     }
 }
